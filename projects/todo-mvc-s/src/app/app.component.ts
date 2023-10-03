8573dd87c664ba9ce6c8c34a61d307c76e607f5e
rdnscr <raphael.ritter@bluewin.ch>
2023-10-03 15:03:26 +0200

    boostrap stand alone components

--- projects/todo-mvc-s/src/app/app.component.ts
+++ projects/todo-mvc-s/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { Route } from '@angular/router';
+import { Route, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgFor } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 /*
  * App Component
  * Top Level Component
  */
 @Component({
-  selector: 'todo-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'todo-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [MatSidenavModule, MatIconModule, MatListModule, RouterLink, NgFor, RouterLinkActive, MatToolbarModule, MatButtonModule, RouterOutlet]
 })
 export class AppComponent {
   public name = '2BIT Angular Demos';
--- projects/todo-mvc-s/src/main.ts
+++ projects/todo-mvc-s/src/main.ts
@@ -1,11 +1,29 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
+import { withPreloading, provideRouter, NoPreloading } from '@angular/router';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, MatIconModule, MatSidenavModule, MatToolbarModule, MatButtonModule, MatListModule),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(appRoutes, withPreloading(NoPreloading)),
+    ]
+})
   .catch(err => console.log(err));
